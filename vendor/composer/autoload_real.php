f3d5b65ad92b2b67403e2fe2a720badf798d167d
Nathan Feitoza <nathanfeitoza@outlook.com.br>
2018-05-16 09:04:10 -0300

    Adicionada Licença MIT e removido PHPUnit, pois a versão minima do PHP não é compatível com a dele

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,24 +47,6 @@ class ComposerAutoloaderInit268b6656a3cd4ac6f1c8011cac962b72
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit268b6656a3cd4ac6f1c8011cac962b72::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire268b6656a3cd4ac6f1c8011cac962b72($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequire268b6656a3cd4ac6f1c8011cac962b72($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
